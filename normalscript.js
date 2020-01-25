e3b1975380ba28199f276a225671583b2fd85ff8
Patrick Roos <pa.roos@icloud.com>
2020-01-25 19:21:33 +0100

    update showcase

--- normalscript.js
+++ normalscript.js
@@ -1,1 +1,3 @@
+let normal = Date.now();
+while (Date.now() < normal + 1000) {}
 console.log('hello from normal script');
--- normalscript2.js
+++ normalscript2.js
@@ -1,1 +1,3 @@
+let normal2 = Date.now();
+while (Date.now() < normal2 + 1000) {}
 console.log('hello from normal script II');
